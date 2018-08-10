c7eb3c211915144edf221eb0daed371fea6e7720
Harry Nicholls <harry.m.nicholls@gmail.com>
2018-08-10 15:17:03 -0400

    Created State Module

--- client/src/app/state/state.module.ts
+++ client/src/app/state/state.module.ts
@@ -1,10 +1,35 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { environment } from '../../environments/environment';
+import { ModuleWithProviders } from '../../../node_modules/@angular/compiler/src/core';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forRoot({ routerReducer }),
+    StoreRouterConnectingModule.forRoot(),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   declarations: []
 })
-export class StateModule { }
+export class StateModule {
+  constructor(@Optional() @SkipSelf() parentModule: StateModule) {
+    if (parentModule) {
+      throw new Error(
+        'StateModule is already loaded. Import it in the AppModule only'
+      );
+    }
+  }
+
+  static forRoot(): ModuleWithProviders {
+     return {
+       ngModule: StateModule,
+       providers: []
+     };
+  }
+}
